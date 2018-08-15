da851c054d998c9e93cd06688e7fa4e74f5ed3e6
Sean Kelly <spkellydev@gmail.com>
2018-08-14 21:02:04 -0400

    router initialize and sending dummy data

--- index.ts
+++ index.ts
@@ -1,22 +1,17 @@
-/**
- * @package Typescript Express WSS API
- * @summary Server implementation of Analytics Dashboard
- * @version 0.0.0
- */
-import * as express from "express";
+import * as debug from "debug";
 import * as http from "http";
 import * as WebSocket from "ws";
-const port = 8080;
 
-const app = express();
+import Server from "./server";
 
-/**
- * (alias) server
- * relies on http.createServer,
- * relies on expreess()
- * initialize a simple http server
- */
-const server = http.createServer(app);
+debug("ts-dash:server");
+
+const port = normalizePort(process.env.PORT || 8080);
+Server.set("port", port);
+
+console.log(`Server listening on port ${port}`);
+
+const server = http.createServer(Server);
 
 /**
  * WebSocket server instance
@@ -95,9 +90,42 @@ setInterval(() => {
   });
 }, 1000).unref(); // allow server to disconnect
 
-//start our server
-server.listen(process.env.PORT || port, () => {
-  // port resolves to string | WebSocket.AddressInfo if not served over TCP
-  const { port } = server.address() as WebSocket.AddressInfo;
-  console.log(`Server started on port ${port} :)`);
-});
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val: number | string): number | string | boolean {
+  const port: number = typeof val === "string" ? parseInt(val, 10) : val;
+  if (isNaN(port)) {
+    return val;
+  } else if (port >= 0) {
+    return port;
+  } else {
+    return false;
+  }
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening(): void {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+}
---
+++ server.ts
@@ -0,0 +1,54 @@
+/**
+ * @package Typescript Express WSS API
+ * @summary Server implementation of Analytics Dashboard
+ * @version 0.0.0
+ */
+import * as cors from "cors";
+import * as http from "http";
+import * as logger from "morgan";
+import * as helmet from "helmet";
+import * as WebSocket from "ws";
+import * as express from "express";
+import * as mongoose from "mongoose";
+import * as bodyParser from "body-parser";
+import * as compression from "compression";
+
+// import routers
+import GoogleRouter from "./routes/GoogleRouter";
+
+const dev = process.env.NODE_ENV != "production";
+
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  config() {
+    const MONGO_URI = "mongodb://localhost:27017/dashboard";
+    mongoose.connect(
+      MONGO_URI || process.env.MONGODB_URI,
+      { useNewUrlParser: true }
+    );
+
+    this.app.use(logger(dev ? "dev" : "combined"));
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
+
+  public routes() {
+    let router: express.Router;
+    router = express.Router();
+
+    this.app.use("/", router);
+    this.app.use("/api", GoogleRouter);
+  }
+}
+
+export default new Server().app;
