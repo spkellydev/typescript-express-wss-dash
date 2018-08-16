35772f84aa3dd96b293d90657cc060c1123659ec
Sean Kelly <spkellydev@gmail.com>
2018-08-15 20:03:19 -0400

    setting up http tests

---
+++ __tests__/unit/http.ts
@@ -0,0 +1,30 @@
+import * as request from "supertest";
+import Server from "../../protocols/HttpServer";
+import * as http from "http";
+
+describe("Test the root path", () => {
+  let port: number;
+  let server: http.Server;
+
+  beforeEach(done => {
+    let port = 4000;
+    Server.set("port", port);
+    server = http.createServer(Server);
+    server.listen(port);
+    done();
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  test("It should response the GET method", done => {
+    request(server)
+      .get("/api/v0/auth/signin")
+      .then(response => {
+        console.log(response.status);
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+});
--- protocols/HttpServer.ts
+++ protocols/HttpServer.ts
@@ -47,7 +47,7 @@ class Server {
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(bodyParser.json());
     this.app.use(compression());
-    this.app.use(helmet());
+    // this.app.use(helmet()); // TODO: Production
     this.app.use(cors());
   }
 
